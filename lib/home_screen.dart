bf0fca73c326679c158a6aa37b2cbe5c6f64773c
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-08-19 03:26:06 +0600

    added question and ans

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,92 +1,127 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:quizzler_flutter/quiz_brain.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<Widget> scoreKeeper = [];
+
+  QuizBrain quizBrain = QuizBrain();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 25, 25, 25),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Center(
-                child: Text(
-                  'This is where the question text will go.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.symmetric(vertical: 15),
-              child: Container(
-                height: 60,
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    log('got clicked');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Center(
                   child: Text(
-                    'True',
+                    quizBrain.getQuestionText(),
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Colors.white,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      fontSize: 24,
                     ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.symmetric(vertical: 15),
-              child: Container(
-                height: 60,
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    log('got clicked');
-                  },
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                  child: Text(
-                    'False',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(vertical: 15),
+                child: Container(
+                  height: 60,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      bool correctAnswer = quizBrain.getQuestionAnswer();
+                      if (correctAnswer == true) {
+                        log('you got it correct');
+                      } else {
+                        log('you got it wrong');
+                      }
+
+                      setState(() {
+                        quizBrain.nextQuestion();
+                        // if (questionsIndex == quizBrain.questionList.length) {
+                        //   questionsIndex = 0;
+                        // }
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    child: Text(
+                      'True',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Row(
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(vertical: 15),
+                child: Container(
+                  height: 60,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      bool correctAnswer = quizBrain.getQuestionAnswer();
+                      if (correctAnswer == false) {
+                        log('you got it correct');
+                      } else {
+                        log('you got it wrong');
+                      }
 
-                children: [
-                Icon(
-                    Icons.check,
-                    color: Colors.green
+                      setState(() {
+                        quizBrain.nextQuestion();
+                        // if (questionsIndex == quizBrain.questionList.length) {
+                        //   questionsIndex = 0;
+                        // }
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    child: Text(
+                      'False',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
                 ),
-                Icon(
-                  Icons.close,
-                  color: Colors.red,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  children: [
+                    Icon(Icons.check, color: Colors.green),
+                    Icon(Icons.close, color: Colors.red),
+                  ],
                 ),
-              ],),
-            ),
-
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
